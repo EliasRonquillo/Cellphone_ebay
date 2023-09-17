99807b10c8b90dfd15d0c8cb501546f2ab595dea
Elias Ronquillo <rl18003@ues.edu.sv>
2023-09-16 22:57:25 -0600

    Layout, toolbar add

---
+++ src/pages/PaginaInicio.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+  <q-page :style-fn="myTweak" padding>
+    <div class="row">
+      <div class="col">.col</div>
+      <div class="col-6">.col-6 (wider)</div>
+      <div class="col">.col</div>
+    </div>
+  </q-page>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,5 @@
-
 const routes = [
+  /*
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
@@ -7,13 +7,19 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
+  */
+  {
+    path: "/",
+    component: () => import("layouts/InicioLayout.vue"),
+    children: [{ path: "", component: () => import("pages/PaginaInicio.vue") }],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
