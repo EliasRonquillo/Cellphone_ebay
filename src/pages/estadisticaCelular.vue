d59931ff0a5412f1ccb08e8c6d4e2ec7e39f3609
Angel <al20034@ues.edu.sv>
2023-11-11 12:49:06 -0600

    Modif..

--- src/pages/estadisticaCelular.vue
+++ src/pages/estadisticaCelular.vue
@@ -5,18 +5,31 @@
         <q-icon name="settings" size="5em" />
       </div>
       <div class="text-h6 text-center">
-        <div class="text-h3">Estadistica</div>
-        Dispositivos registrados en la base de datos
-      
+        
+         <div class="q-pa-md">
+    <q-img
+      src="https://static.vecteezy.com/system/resources/previews/002/158/616/non_2x/resume-of-the-employee-icon-elements-of-hr-and-heat-hunting-in-neon-style-icons-simple-icon-for-websites-web-design-mobile-app-info-graphics-isolated-on-brick-wall-background-vector.jpg"
+      spinner-color="white"
+      style="height: 170px; max-width: 300px"
+      img-class="my-custom-image"
+      class="rounded-borders"
+    >
+      <div class="absolute-bottom text-subtitle1 text-center">
         <div v-if="todos.length > 0">
-          <q-icon name="analytics" size="2em" />
-          <p>Total de elementos: {{ todos.length }}</p>
+          <p>Total de Dispositos: {{ todos.length }}</p>
         </div>
         <div v-else>
           Cargando...
         </div>
+      </div>
+    </q-img>
+  </div>
+
+      
         
       </div>
+
+   
     </q-page-container>
   </q-page>
 </template>
